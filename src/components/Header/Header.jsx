7f97336b8155725486a11e1d3954d6b6c984ad05
Yurii Chikalo <yurii.chicalo@jetsoftpro.com>
2022-07-20 14:30:41 +0300

    init commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+function Header() {
+    return (
+        <div className={styles.container}>
+            <div>header</div>
+        </div>
+    );
+}
+
+export default Header;
